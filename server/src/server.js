a4427d0d5e05cd59f09255c737e9845b88e92fef
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-11-09 13:09:00 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CLIENT_ORIGIN || 'https://note-app-gamma-eight.vercel.app',
   credentials: true
 }));
 app.use((req, res, next) => {
